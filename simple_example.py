148f3d0ba4ee5aee5ad99ef4aff7fb2c0b800cc7
ruachang <lcfatego777@163.com>
2022-12-08 11:47:49 +0000

    基本示例

---
+++ simple_example.py
@@ -0,0 +1,55 @@
+from nvidia.dali.pipeline import Pipeline
+import nvidia.dali.fn as fn
+import nvidia.dali.ops as ops
+import nvidia.dali.types as types
+import numpy as np
+
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+
+txt_path = "/home/developers/liuchang/FER数据集/Oulu-CASIA/NI200cropped/10_fold_txt/0_10_fold_train.txt".format(10)
+image_dir = "/home/developers/liuchang/FER数据集"
+batch_size = 8
+
+
+def show_images(image_batch):
+    columns = 4
+    rows = (batch_size + 1) // (columns)
+    fig = plt.figure(figsize = (32,(32 // columns) * rows))
+    gs = gridspec.GridSpec(rows, columns)
+    for j in range(rows*columns):
+        plt.subplot(gs[j])
+        plt.axis("off")
+        plt.imshow(image_batch.at(j))
+    plt.show()
+    plt.savefig("./try.png")
+
+
+class SimplePipeline(Pipeline):
+    def __init__(self, batch_size, num_threads, device_id):
+        super(SimplePipeline, self).__init__(batch_size, num_threads, device_id, seed = 12)
+        # self.input = ops.FileReader(file_root = image_dir, random_shuffle=True, initial_fill=16)
+        # instead of path to file directory file with pairs image_name image_label_value can be provided
+        # * 读取
+        self.input = ops.FileReader(file_root = image_dir, file_list = txt_path)
+        # * 解码
+        self.decode = ops.ImageDecoder(device = 'mixed', output_type = types.RGB)
+        # * 随机裁剪(预处理)
+        self.resizedCrop = ops.RandomResizedCrop(device="gpu", size=224, random_area=[0.08, 1.25])
+    # * 设定Pipeline的执行顺序
+    def define_graph(self):
+        jpegs, labels = self.input()
+        images = self.decode(jpegs)
+        images = self.resizedCrop(images)
+        return (images, labels)
+# * 初始化Pipeline类
+pipe = SimplePipeline(batch_size, 1, 0)
+pipe.build()
+# * 从pipe中读取一个batch
+pipe_out = pipe.run()
+print(pipe_out)
+# * 可视化
+images, labels = pipe_out
+# 由于image已经被放到GPU上, 所以无法直接显示
+show_images(images.as_cpu())
+
